ae8e780f4c4dc28c82d79b34d2bb406c53903464
Greg Palmer <gpalmer323@gmail.com>
2013-05-27 21:05:08 -0700

    color slider in statsview

--- www/compiled/app/views/statsView.js
+++ www/compiled/app/views/statsView.js
@@ -14,6 +14,7 @@
 
     StatsView.prototype.template = _.template('\
     <span class="count">n = <%= n %>, # active cells: <%= aliveCells %></span>\
+    <div class="slider color"><input id="color" type="range" min="0" max="255" value="127" /></div>\
   ');
 
     StatsView.prototype.initialize = function() {
